88fab4b7a346e6618d3a98cfd5174c5e09dc1b6c
karolwk <krl@o2.pl>
2022-10-02 20:02:17 +0200

    added icons collection

--- src/collections/mainAmenitiesCollection.ts
+++ src/collections/mainAmenitiesCollection.ts
@@ -1,18 +1,38 @@
-import {
-  buildCollection,
-  buildProperty,
-  EntityReference,
-} from '@camberi/firecms';
+import { buildCollection, EntityReference } from '@camberi/firecms';
 
 type MainAmenities = {
   name: string;
-  icons: string[];
+  icon: EntityReference;
 };
 
 type Icons = {
   name: string;
   iconPath: string;
 };
+
+export const iconsCollection = buildCollection<Icons>({
+  name: 'IkonA',
+  path: 'icons',
+  description: 'Dodaj ikony',
+  group: 'dodatkowe',
+  hideFromNavigation: true,
+  properties: {
+    name: {
+      name: 'Nazwa ikony',
+      dataType: 'string',
+    },
+    iconPath: {
+      name: 'Obrazek ikony',
+      dataType: 'string',
+
+      storage: {
+        storagePath: 'icons',
+        acceptedFiles: ['image/*'],
+        maxSize: 1024 * 1024,
+      },
+    },
+  },
+});
 
 export const mainAmenitiesCollection = buildCollection<MainAmenities>({
   name: 'Główne udogodnienia',
@@ -23,10 +43,15 @@ export const mainAmenitiesCollection = buildCollection<MainAmenities>({
   group: 'dodatkowe',
   properties: {
     name: {
-      name: 'rodzaj udogodnienia',
+      name: 'Nazwa udogodnienia',
       validation: { required: true },
       dataType: 'string',
     },
-    icons: {},
+    icon: {
+      dataType: 'reference',
+      path: 'icons',
+      name: 'Ikona',
+      description: 'Wybierz ikonę opisującą udogodnienie',
+    },
   },
 });
