7082faecf34956f89e3c18b972778f7af683ae28
karolwk <krl@o2.pl>
2022-10-06 21:19:14 +0200

    added types

--- src/collections/mainPageCollection.ts
+++ src/collections/mainPageCollection.ts
@@ -3,3 +3,25 @@ import {
   buildProperty,
   EntityReference,
 } from '@camberi/firecms';
+
+type marKetingIcons = {
+  iconURL: string;
+  title: string;
+  description: string;
+};
+
+type faqMainPage = {
+  question: string;
+  answer: string;
+};
+
+type mainPageCollection = {
+  title: string;
+  description: string;
+  icons: marKetingIcons[];
+  teaserTitle: string;
+  teaserContent: string;
+  campersTitle: string;
+  campersDescription: string;
+  faq: faqMainPage[];
+};
