128db72a4c7c539401fffb3abd1bf088741c993c
karolwk <krl@o2.pl>
2022-10-10 20:29:36 +0200

    added settings collection

---
+++ src/collections/settingsCollection.ts
@@ -0,0 +1,64 @@
+import {
+  buildCollection,
+  buildProperty,
+  EntityReference,
+} from '@camberi/firecms';
+
+type PageSettings = {
+  logoURL: string;
+  email: string;
+  phone: string;
+  companyName: string;
+  companyaddress: string;
+  maps?: 'Google' | 'SnazzyMaps';
+};
+
+export const pageSettingsCollection = buildCollection<PageSettings>({
+  name: 'Ustawienia strony',
+  description: 'Zmień główne ustawienia strony',
+  path: 'settings',
+  properties: {
+    logoURL: {
+      name: 'Wybierz logo',
+      dataType: 'string',
+      storage: {
+        storagePath: 'images',
+        acceptedFiles: ['image/*'],
+        metadata: {
+          cacheControl: 'max-age=1000000',
+        },
+      },
+    },
+    email: {
+      name: 'Podaj e-mail',
+      dataType: 'string',
+      validation: { required: true },
+    },
+    phone: {
+      name: 'Podaj nr telefonu',
+      dataType: 'string',
+      validation: { required: true },
+    },
+    companyName: {
+      name: 'Podaj dane firmy/osoby',
+      dataType: 'string',
+    },
+    companyaddress: {
+      name: 'Podaj adres siedziby',
+      dataType: 'string',
+      multiline: true,
+    },
+    maps: buildProperty({
+      name: 'Wybierz mape',
+      description: 'Wybierz dostawcę mapki która będzie używana na stronie',
+      dataType: 'array',
+      oneOf:{
+        typeField: 'type',
+        valueField: 'value',
+        properties:{
+        
+        }
+
+      }
+    }
+);
