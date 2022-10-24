3c1cb3bdd9274aee090ea8e2c5c2533d4a781bc1
karolwk <krl@o2.pl>
2022-10-24 19:35:41 +0200

    added new ui component

---
+++ src/components/ui/TopView.tsx
@@ -0,0 +1,35 @@
+import { Box, Container, Grid, Typography } from '@mui/material';
+
+type Props = {
+  children: React.ReactNode;
+  title: string;
+};
+
+const TopView = ({ children, title }: Props) => {
+  return (
+    <Box display="flex" width={'100%'} height="100%">
+      <Box
+        display="flex"
+        flexDirection={'column'}
+        alignItems={'center'}
+        justifyItems={'center'}
+      >
+        <Container
+          maxWidth={'md'}
+          sx={{
+            my: 4,
+          }}
+        >
+          <Grid container rowSpacing={5} columnSpacing={2}>
+            <Grid item xs={12}>
+              <Typography variant={'h4'}>{title}</Typography>
+            </Grid>
+            {children}
+          </Grid>
+        </Container>
+      </Box>
+    </Box>
+  );
+};
+
+export default TopView;
