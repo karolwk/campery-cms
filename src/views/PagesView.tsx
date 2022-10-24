360754b92ee6b38d78ca187a8ef02e89cc096cf3
karolwk <krl@o2.pl>
2022-10-24 19:35:25 +0200

    added logic

--- src/views/PagesView.tsx
+++ src/views/PagesView.tsx
@@ -23,5 +23,43 @@ import {
   useSideEntityController,
   useSnackbarController,
 } from '@camberi/firecms';
-import { pageSettingsCollection } from '../collections/settingsCollection';
+
 import ViewButton from '../components/ui/ViewButton';
+import TopView from '../components/ui/TopView';
+import { mainPageCollection } from '../collections/mainPageCollection';
+
+type Props = {};
+
+const PagesView = (props: Props) => {
+  const contactsEntityController = useSideEntityController();
+
+  const mainPageEntityHandler = () =>
+    contactsEntityController.open({
+      entityId: 'wZd7hHxY6F6AAU3uw7Ru',
+      path: '/mainPage',
+      collection: mainPageCollection,
+      width: 800,
+    });
+
+  return (
+    <TopView title="Edytuj strony">
+      <ViewButton
+        onClick={mainPageEntityHandler}
+        content="Stronę główna"
+        buttonText="Zmień"
+      />
+      <ViewButton
+        onClick={mainPageEntityHandler}
+        content="Strona 'O nas'"
+        buttonText="Zmień"
+      />
+      <ViewButton
+        onClick={mainPageEntityHandler}
+        content="Strona 'Warunki wynajmu'"
+        buttonText="Zmień"
+      />
+    </TopView>
+  );
+};
+
+export default PagesView;
