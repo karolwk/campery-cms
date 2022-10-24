11b489235ffe31286ef7739bf830982c03aa8ee8
karolwk <krl@o2.pl>
2022-10-24 19:35:53 +0200

    added grid wrap

--- src/components/ui/ViewButton.tsx
+++ src/components/ui/ViewButton.tsx
@@ -2,6 +2,7 @@ import {
   Button,
   Card,
   CardActions,
+  Grid,
   CardContent,
   Typography,
 } from '@mui/material';
@@ -30,17 +31,19 @@ const ViewButton = ({
   children,
 }: Props) => {
   return (
-    <Card variant="outlined" sx={{ height: '100%' }}>
-      <CardContent>
-        <Typography>{content}</Typography>
-      </CardContent>
+    <Grid item xs={12} sm={4}>
+      <Card variant="outlined" sx={{ height: '100%' }}>
+        <CardContent>
+          <Typography>{content}</Typography>
+        </CardContent>
 
-      <CardActions>
-        <Button onClick={onClick} color={color}>
-          {!children ? buttonText : children}
-        </Button>
-      </CardActions>
-    </Card>
+        <CardActions>
+          <Button onClick={onClick} color={color}>
+            {!children ? buttonText : children}
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 };
 
