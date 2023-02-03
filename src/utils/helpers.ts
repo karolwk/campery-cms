7565604cfedfe2279522e6ca20f2bf71135894dc
karolwk <krl@o2.pl>
2023-02-03 13:39:24 +0100

    added firstLowercase helper method

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,10 @@
 export const makeURLfromName = (name: string): string => {
   return name.trim().toLowerCase().replace(/ /g, '-');
 };
+
+export const firstLetterLowercase = (name: string): string => {
+  if (name.length < 2) {
+    return name;
+  }
+  return name.replace(' ', '_').toLowerCase();
+};
