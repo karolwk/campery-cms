2c851d81725263f10772c190bd20dfd49a9c11e3
karolwk <krl@o2.pl>
2023-03-09 23:29:42 +0100

    new helper function to get token

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,6 @@
+import { FireCMSContext } from '@camberi/firecms';
+import { tokensCollection } from '../collections/tokensCollections';
+
 export const makeURLfromName = (name: string): string => {
   return name.trim().toLowerCase().replace(/ /g, '-');
 };
@@ -7,4 +10,18 @@ export const firstLetterLowercase = (name: string): string => {
     return name;
   }
   return name.replace(' ', '_').toLowerCase();
+};
+
+// Get's token from database
+export const getToken = async (context: FireCMSContext) => {
+  try {
+    const res = await context.dataSource.fetchCollection({
+      path: 'tokens',
+      collection: tokensCollection,
+    });
+    return res[0].values.isrToken;
+  } catch (error) {
+    console.log(error);
+    return '';
+  }
 };
